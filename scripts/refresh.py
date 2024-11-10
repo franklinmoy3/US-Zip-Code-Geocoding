4f2216ec87c4c78c765e937d7cf1a857eaf9dd6c
Franklin Moy <franklinmoy3@gmail.com>
2024-11-10 19:10:32 +0000

    Add dict version of condensed US data

--- scripts/refresh.py
+++ scripts/refresh.py
@@ -13,6 +13,7 @@ compressed_data_file_path = os.path.join(data_path, "US.zip")
 extracted_data_path = os.path.join(data_path, "US")
 data_file_path = os.path.join(extracted_data_path, "US.txt")
 transformed_data_file_name = os.path.join(data_path, "US-condensed.json")
+mapped_data_file_name = os.path.join(data_path, "US-condensed-mapped.json")
 
 
 # Refresh by pulling from Geonames
@@ -27,6 +28,7 @@ with zipfile.ZipFile(compressed_data_file_path, 'r') as zf:
 
 # Transform
 data = []
+data_as_map = {}
 with open(data_file_path, 'r') as tsv_f:
     tsv_reader = csv.reader(tsv_f, delimiter="\t")
     for row in tsv_reader:
@@ -46,5 +48,24 @@ with open(data_file_path, 'r') as tsv_f:
         }
         data.append(postal_code_info)
 
+        postal_code_info_as_map_value = {
+            # "countryCode": row[0],
+            # "postalCode": row[1],
+            "city": row[2],
+            # "adminName1": row[3],
+            "state": row[4],
+            # "adminName2": row[5],
+            # "adminCode2": row[6],
+            # "adminName3": row[7],
+            # "adminCode3": row[8],
+            "latitude": float(row[9]),
+            "longitude": float(row[10]),
+            # "accuracy": row[11],
+        }
+        data_as_map[row[1]] = postal_code_info_as_map_value
+
 with open(transformed_data_file_name, 'w') as f:
     json.dump(data, f, indent=2)
+
+with open(mapped_data_file_name, "w") as f:
+    json.dump(data_as_map, f, indent=2)
