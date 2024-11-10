baaed5a7a8b0bd7c63478e34e5acf4afcb8ed929
Franklin Moy <franklinmoy3@gmail.com>
2024-11-10 19:04:03 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,39 @@
+# US ZIP Code Geocoding <!-- omit in toc -->
+
+This repository contains a list of all ZIP codes in the US along with their approximate coordinates.
+
+- [License](#license)
+  - [Scripts License](#scripts-license)
+  - [Dataset License](#dataset-license)
+- [Credits](#credits)
+
+## License
+
+This repository is licensed under multiple licenses:
+
+### Scripts License
+
+The scripts located in the [`scripts`](./scripts/) directory is licensed under an MIT License. See [scripts/LICENSE](./scripts/LICENSE) for more details.
+
+### Dataset License
+
+"This dataset" refers to the [`data`](./data/) directory.
+
+This dataset is licensed under a [Creative Commons Attribution 4.0 International License](./data/LICENSE).
+
+This dataset is provided "as is" without warranty or representation of accuracy, timeliness, or completeness.
+
+You are free to use this dataset as long as you provide credit the following parties: 
+
+- Geonames
+  - Author of original dataset
+  - Must provide a link to https://www.geonames.org 
+- Franklin Moy
+  - Author of modifications
+  - Must provide a link to this repository (https://github.com/franklinmoy3/US-Zip-Code-Geocoding)
+
+See https://creativecommons.org/licenses/by/4.0/ for more details.
+
+## Credits
+
+This data is based off of the Geonames Postal Code dataset from Geonames Gazeeter data. See https://www.geonames.org
---
+++ scripts/refresh.py
@@ -0,0 +1,50 @@
+import csv
+import json
+import os
+import requests
+import subprocess
+import zipfile
+
+
+repo_root = subprocess.run(
+    ["git", "rev-parse", "--show-toplevel"], stdout=subprocess.PIPE, text=True).stdout.strip()
+data_path = os.path.join(repo_root, "data")
+compressed_data_file_path = os.path.join(data_path, "US.zip")
+extracted_data_path = os.path.join(data_path, "US")
+data_file_path = os.path.join(extracted_data_path, "US.txt")
+transformed_data_file_name = os.path.join(data_path, "US-condensed.json")
+
+
+# Refresh by pulling from Geonames
+compressed_data = requests.get(
+    "https://download.geonames.org/export/zip/US.zip")
+
+with open(compressed_data_file_path, "wb+") as f:
+    f.write(compressed_data.content)
+
+with zipfile.ZipFile(compressed_data_file_path, 'r') as zf:
+    zf.extractall(extracted_data_path)
+
+# Transform
+data = []
+with open(data_file_path, 'r') as tsv_f:
+    tsv_reader = csv.reader(tsv_f, delimiter="\t")
+    for row in tsv_reader:
+        postal_code_info = {
+            # "countryCode": row[0],
+            "postalCode": row[1],
+            "city": row[2],
+            # "adminName1": row[3],
+            "state": row[4],
+            # "adminName2": row[5],
+            # "adminCode2": row[6],
+            # "adminName3": row[7],
+            # "adminCode3": row[8],
+            "latitude": float(row[9]),
+            "longitude": float(row[10]),
+            # "accuracy": row[11],
+        }
+        data.append(postal_code_info)
+
+with open(transformed_data_file_name, 'w') as f:
+    json.dump(data, f, indent=2)
